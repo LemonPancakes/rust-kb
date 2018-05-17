e4a3d91aac54450acb3cb8ae257fc4e4c75bda99
ChristopherKober <ChristopherKober@users.noreply.github.com>
2018-05-17 15:53:52 -0500

    Added untested parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+use pest::*;
+
+#[cfg(debug_assertions)]
+const _GRAMMAR: &'static str = include_str!("grammar.pest");
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+pub struct PDDLParser;
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let x = PDDLParser::parse(Rule::domain,"(define (Domain testing) \
+    (:requirements :strips :equality)\
+    (:types ad bd cd - dd)\
+    (:constants ad bd cd - dd)\
+    (:predicates (PREDICATE_1_NAME ?A1 ?A2 ?AN) (PREDICATE_2_NAME ?A1 ?A2 ?AN)))\
+    (:functions (distance ?from ?to) (total-cost))\
+    (:constraints (and (always (clean truck1)) (always (clean truck1)) (at end (at package2 paris))))\
+    ").unwrap();
+
+    println!("{}",x);
 }
