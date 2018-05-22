7a2a35dabbd10b34486a24df7387a79dda741f47
brotatotes <hao.awesome@gmail.com>
2018-05-22 16:28:34 -0500

    move kb files to test

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,6 @@ mod kb;
 use kb::parser::parse_kb_from_file;
 
 fn main() {
-    let kb = parse_kb_from_file("src/full.kb").unwrap();
+    let kb = parse_kb_from_file("test/full.kb").unwrap();
     println!("{:?}", kb);
 }
